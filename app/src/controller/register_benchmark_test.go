78a3325aedd46b8c79c873413787d43dfba414a3
zach <bui.lam.quang.ngoc@moneyforward.vn>
2025-03-27 20:32:35 +0700

    rename benchmark test files

---
+++ app/src/controller/register_benchmark_test.go
@@ -0,0 +1,61 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func BenchmarkRegister(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fullname := fmt.Sprintf("test%s", uuid.New().String())
+		username := fmt.Sprintf("test%s", uuid.New().String())
+		password := "password"
+		callApiRegister(b, fullname, username, password)
+	}
+}
+
+func callApiRegister(b *testing.B, fullname, email, password string) {
+	// Create a request body
+	requestBody := map[string]interface{}{
+		"fullname": fullname,
+		"email":    email,
+		"password": password,
+	}
+
+	// Convert the body to JSON
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		b.Fatalf("Failed to marshal JSON: %v", err)
+	}
+
+	// Create a new HTTP POST request
+	req, err := http.NewRequest("POST", "http://amfa-test:8083/v1/auth/register", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		b.Fatalf("Failed to create request: %v", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create an HTTP client and send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		b.Fatalf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read and process the response if needed
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		b.Fatalf("Failed to read response: %v", err)
+	}
+}
+
+
